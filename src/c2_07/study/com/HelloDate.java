1ac2f430b233b6bd4504e4429cd0780da90916b2
WXB506 <1922686887@qq.com>
2016-03-20 10:17:34 +0800

    add hellodate.java

---
+++ src/c2_07/study/com/HelloDate.java
@@ -0,0 +1,10 @@
+package c2_07.study.com;
+
+import java.util.*;
+
+public class HelloDate {
+	public static void main(String[] args) {
+		System.out.print("Hello, it's : ");
+		System.out.println(new Date());
+	}
+}
