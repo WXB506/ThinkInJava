292700509591c8a212e2491e9bd22e95dfc94c9c
WXB506 <1922686887@qq.com>
2016-03-22 23:20:57 +0800

    default constructor

---
+++ src/c07_02/study/com/Cartoon.java
@@ -0,0 +1,23 @@
+package c07_02.study.com;
+
+class Art {
+	Art() {
+		System.out.println("Art()");
+	}
+}
+
+class Drawing extends Art {
+	Drawing() {
+		System.out.println("Drawing()");
+	}
+}
+
+public class Cartoon extends Drawing {
+	public Cartoon() {
+		System.out.println("Cartoon()");
+	}
+	
+	public static void main(String[] args) {
+		Cartoon x = new Cartoon();
+	}
+}
