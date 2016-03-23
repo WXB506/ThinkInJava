e39c79f3cb2f4bb2b107cab806371a55832998c7
WXB506 <1922686887@qq.com>
2016-03-23 22:00:42 +0800

    delegate

---
+++ src/c07_03/study/com/SpaceShipControls.java
@@ -0,0 +1,19 @@
+package c07_03.study.com;
+
+public class SpaceShipControls {
+	void up(int velocity) {
+	}
+	void down(int velocity) {
+	}
+	void left(int velocity) {
+	}
+	void right(int velocity) {
+	}
+	void forward(int velocity) {
+		System.out.println(velocity);
+	}
+	void back(int velocity) {
+	}
+	void turboBoost() {
+	}
+}
