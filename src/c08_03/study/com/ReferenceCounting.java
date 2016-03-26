f08e417b2dfb29d78cd9e47e959d10e78a9309ac
WXB506 <1922686887@qq.com>
2016-03-26 21:50:56 +0800

    reference counting

---
+++ src/c08_03/study/com/ReferenceCounting.java
@@ -0,0 +1,58 @@
+package c08_03.study.com;
+
+class Shared {
+	private int refcount = 0;
+	private static long counter = 0;
+	private final long id = counter++;
+	
+	public Shared() {
+		System.out.println("Creating " + this);
+	}
+	public void addRef() {
+		refcount++;
+	}
+	protected void dispose() {
+		if(--refcount == 0) {
+			System.out.println("Disposing " + this);
+		}
+	}
+	public String toString() {
+		return "Shared " + id;
+	}
+}
+
+class Composing {
+	private Shared shared;
+	private static long counter = 0;
+	private final  long id = counter++;
+	
+	public Composing(Shared shared) {
+		this.shared = shared;
+		this.shared.addRef();
+		System.out.println("Creating " + this);
+	}
+	protected void dispose() {
+		System.out.println("disposing " + this);
+		shared.dispose();
+	}
+	public String toString() {
+		return "Composing " + id;
+	}
+}
+
+public class ReferenceCounting {
+	public static void main(String[] args) {
+		Shared shared = new Shared();
+		Composing[] composings = {
+				new Composing(shared),
+				new Composing(shared),
+				new Composing(shared),
+				new Composing(shared),
+				new Composing(shared),
+				new Composing(shared)
+		};
+		
+		for(Composing c : composings)
+			c.dispose();
+	}
+}
