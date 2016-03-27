b40dcd1ff4eaa1b8aa84783a66b3113f307fa18d
WXB506 <1922686887@qq.com>
2016-03-27 10:54:03 +0800

    interface extends

--- src/c09_04/study/com/InterfaceInhe.java
+++ src/c09_04/study/com/InterfaceInhe.java
@@ -10,6 +10,28 @@ interface layer2 extends base {
 	void c();
 }
 
-public class InterfaceInhe {
-
+interface layer3 extends layer1, layer2 {
+	void d();
+}
+public class InterfaceInhe implements layer3 {
+	public void a() {
+		System.out.println("a ");
+	}
+	public void b() {
+		System.out.println("b");
+	}
+	public void c() {
+		System.out.println("c");
+	}
+	public void d() {
+		System.out.println("d");
+	}
+	
+	public static void main(String[] args) {
+		InterfaceInhe x = new InterfaceInhe();
+		x.a();
+		x.b();
+		x.c();
+		x.d();
+	}
 }
