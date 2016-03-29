760d94c3ddbcb66bc6b2acc99bde3bca572a4505
WXB506 <1922686887@qq.com>
2016-03-29 23:36:08 +0800

    simple

---
+++ src/c11_02/study/com/SimpleCollection.java
@@ -0,0 +1,15 @@
+package c11_02.study.com;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class SimpleCollection {
+	public static void main(String[] args) {
+		Collection<Integer> c = new ArrayList<Integer>();
+		for(int i = 0; i < 10; i++)
+			c.add(i);
+		
+		for(Integer integer : c)
+			System.out.println(integer);
+	}
+}
