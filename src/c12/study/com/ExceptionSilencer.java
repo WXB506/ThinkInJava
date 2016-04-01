a145763047c1f0ef0e60fb987a02a0f58f6ed42a
WXB506 <1922686887@qq.com>
2016-04-01 08:08:11 +0800

    lost exception

---
+++ src/c12/study/com/ExceptionSilencer.java
@@ -0,0 +1,11 @@
+package c12.study.com;
+
+public class ExceptionSilencer {
+	public static void main(String[] args) {
+		try {
+			throw new RuntimeException();
+		} finally {
+			return;
+		}
+	}
+}
