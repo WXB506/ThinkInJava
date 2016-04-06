e69f7715a83c947ee29be0aaf70389f8bb0d1bf8
WXB506 <1922686887@qq.com>
2016-04-06 21:37:35 +0800

    generic generator

---
+++ src/c15/study/com/BasicGenerator.java
@@ -0,0 +1,20 @@
+package c15.study.com;
+
+public class BasicGenerator<T> implements Generator<T> {
+	private Class<T> type;
+	
+	public BasicGenerator(Class<T> type) {
+		this.type = type;
+	}
+	public T next() {
+		try {
+			return type.newInstance();
+		} catch(Exception e) {
+			throw new RuntimeException();
+		}
+	}
+	
+	public static <T> Generator<T> create(Class<T> type) {
+		return new BasicGenerator<>(type);
+	}
+}
