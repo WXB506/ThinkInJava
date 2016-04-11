0af1b3f80d63f70cc826f73cc3533bf0c5c75c2a
WXB506 <1922686887@qq.com>
2016-04-11 21:50:19 +0800

    getDeclaredMethods() & getAnnotation()

--- src/c20/study/com/PasswordUtils.java
+++ src/c20/study/com/PasswordUtils.java
@@ -7,7 +7,7 @@ public class PasswordUtils {
 	public boolean validatePassword(String password) {
 		return (password.matches("\\w*\\d\\w*"));
 	}
-	@UseCase(id = 47)
+	@UseCase(id = 48)
 	public String encryptPassword(String password) {
 		return new StringBuilder(password).reverse().toString();
 	}
